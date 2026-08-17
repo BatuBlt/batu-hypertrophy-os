d7ed8637d4d20090a62a2f79bc369de27059048c
BatuBlt <batubulut0506@gmail.com>
2026-08-18 00:39:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='batu-hypertrophy-v62-icon';
-const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='batu-hypertrophy-v62-iconfix-icon';
+const CORE=['./','./index.html','./manifest.webmanifest','./icons/batu-icon-v62-180.png','./icons/batu-icon-v62-192.png','./icons/batu-icon-v62-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
